fd08e5ab72de613c9228c17364a8ce7483897496
ericantosch <ericantosch@web.de>
2023-05-26 15:48:21 +0200

    Finished Praktikum 3

--- prak2/gaEinfach.m
+++ prak2/gaEinfach.m
@@ -8,3 +8,24 @@ function [w, track] = gaEinfach(gradFunc, eta, w0, nIts)
     end
 end
 
+H = 100;
+lambda = 0;
+options = trainingOptions('rmsprop',...
+   'ValidationData', {normTrainMat, adsTrain.Labels}, ...
+   'MaxEpochs', 1000, ...
+   'MinibatchSize', 64, ...
+   'L2Regularization', lambda, ...
+   'Verbose', true, ...
+   'Plots', 'none'); % training-progress'); 
+
+layers = [featureInputLayer(P)
+   fullyConnectedLayer(H)
+   reluLayer
+   fullyConnectedLayer(H)
+   reluLayer
+   fullyConnectedLayer(C)
+   softmaxLayer
+   classificationLayer];
+tic
+net = trainNetwork(normTrainMat, adsTrain.Labels, layers, options);
+toc
